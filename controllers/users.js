7997a5a9b41aa76b378bb95c0f6190bc2e97a507
ManliestBen <benjamintmanley@gmail.com>
2020-12-08 09:34:19 -0600

    add profile page

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 
 module.exports = {
   index,
+  showProfile
 };
 
 function index(req, res) {
@@ -9,3 +10,16 @@ function index(req, res) {
     res.render("users/index", { title: "User Index", user: req.user, users });
   });
 }
+
+function showProfile(req, res) {
+  User.findById(req.user._id)
+  .then((user) => {
+    res.render("users/profile", {title: "Profile Page", user})
+  })
+}
+
+// function showProfile(req, res) {
+//   User.findById(req.user._id, function(err, user) {
+//     res.render("users/profile", {title: "Profile Page", user})
+//   })
+// }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const usersCtrl = require("../controllers/users");
 
 router.get("/", isLoggedIn, usersCtrl.index);
+router.get("/profile", isLoggedIn, usersCtrl.showProfile)
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
