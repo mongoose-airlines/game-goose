a70036901d06d4f26ac38bcbdfde57baf404ee99
ManliestBen <benjamintmanley@gmail.com>
2020-12-09 09:01:24 -0600

    add new game page

--- controllers/games.js
+++ controllers/games.js
@@ -1,5 +1,14 @@
 const Game = require('../models/game')
+const axios = require('axios')
 
 module.exports = {
-  
+  new: newGame
+}
+
+function newGame(req, res) {
+  res.render("games/new", {
+    title: "Game Search",
+    user: req.user,
+    results: null
+  })
 }
--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const gamesCtrl = require('../controllers/games')
 
+router.get('/new', isLoggedIn, gamesCtrl.new)
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
